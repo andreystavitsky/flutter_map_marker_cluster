8e02cde0650a9079179b8a9fc0719db13bb09fb5
Andrey <37752638+andreystavitsky@users.noreply.github.com>
2020-07-09 23:17:50 +0300

    deps update

--- example/ios/Flutter/flutter_export_environment.sh
+++ example/ios/Flutter/flutter_export_environment.sh
@@ -1,10 +1,11 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
-export "FLUTTER_ROOT=E:\Projects\_Flutter"
-export "FLUTTER_APPLICATION_PATH=E:\Projects\_Flutter_Projects\flutter_map_marker_cluster\example"
+export "FLUTTER_ROOT=D:\Projects\_Flutter"
+export "FLUTTER_APPLICATION_PATH=D:\Projects\_RoadRocks\flutter_map_marker_cluster\example"
 export "FLUTTER_TARGET=lib\main.dart"
 export "FLUTTER_BUILD_DIR=build"
 export "SYMROOT=${SOURCE_ROOT}/../build\ios"
-export "FLUTTER_FRAMEWORK_DIR=E:\Projects\_Flutter\bin\cache\artifacts\engine\ios"
+export "OTHER_LDFLAGS=$(inherited) -framework Flutter"
+export "FLUTTER_FRAMEWORK_DIR=D:\Projects\_Flutter\bin\cache\artifacts\engine\ios"
 export "FLUTTER_BUILD_NAME=1.0.0"
 export "FLUTTER_BUILD_NUMBER=1"
